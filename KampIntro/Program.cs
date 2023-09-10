f7ea98f620bc21aea8839c230a0e9f5511eb1e3d
cagrisert <cagrisert@belsis.com.tr>
2023-09-10 12:05:42 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(10);
+            myList.Add(100);
+            myList.Add(1000);
+            myList.Add(10000);
+            Console.WriteLine(myList.Count);
+        }
+    }
+
+
+    class MyList<T>
+    {
+
+        T[] _array;
+        T[] _tempArray;
+
+        public MyList()
+        {
+            _array = new T[0];
+        }
+
+
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_tempArray.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+
+            _array[_array.Length - 1] = item;
+        }
+
+        public int Count { get { return _array.Length; } }
+
+    }
+}
